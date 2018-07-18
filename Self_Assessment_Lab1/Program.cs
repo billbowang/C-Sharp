89699c2415ca9fe7046a3386b0fe9d3d75c6d037
Bill Wang <billbowang@gmail.com>
2018-07-18 21:29:28 +0800

    添加项目文件。

---
+++ Self_Assessment_Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Assessment_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentFirstName = "";
+            string studentLastName = "";
+            DateTime studentBirthDate;
+            string studentAddr1 = "";
+            string studentAddr2 = "";
+            string studentCity = "";
+            string studentState = "";
+            string studentZIP = "";
+            string studentCounty = "";
+
+            Console.WriteLine("Please input your name:");
+            studentFirstName = Console.ReadLine();
+            Console.WriteLine("Your name is {0}", studentFirstName);
+            Console.WriteLine($"Your name is {studentFirstName}");
+            Console.Out.WriteLine("Test");
+        }
+    }
+}
